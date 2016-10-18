30970f85afeaed2b2b508c9e3291f2324e5686e3
Mobieus Jay <james.tang@tunebox.com.my>
2016-10-18 07:32:17 +0000

    First commit

---
+++ send_mail.go
@@ -0,0 +1,65 @@
+package main
+
+import (
+	"net/smtp"
+	"fmt"
+	"strings"
+)
+
+
+/**
+	Modified from https://gist.github.com/jpillora/cb46d183eca0710d909a
+	Thank you very much.
+**/
+
+
+const (
+	SMTP_SERVER = "smtp.gmail.com"
+)
+
+
+type Sender struct {
+
+	User		string
+	Password	string
+}
+
+
+func NewSender(Username, Password string) Sender {
+
+	return Sender{Username, Password}
+}
+
+
+func (sender Sender) SendMail(Dest []string, Subject, bodyMessage string) {
+
+	msg := 	"From: " + sender.User + "\n" +
+	       	"To: " + strings.Join(Dest, ",") + "\n" +
+		"Subject: " + Subject + "\n" + bodyMessage
+
+	err := smtp.SendMail(SMTP_SERVER + ":587", 
+		smtp.PlainAuth("", sender.User, sender.Password, SMTP_SERVER),
+		sender.User, Dest, []byte(msg))	
+	
+	if err != nil {
+
+		fmt.Printf("smtp error: %s", err)
+		return
+	}
+
+	fmt.Println("Mail sent successfully!")
+}
+
+
+func main() {
+
+	sender := NewSender("<YOUR EMAIL ADDRESS>", "<YOUR EMAIL PASSWORD>")
+
+	//The receiver needs to be in slice as the receive supports multiple receiver
+	Receiver := []string{"abc@gmail.com", "xyz@gmail.com", "larrypage@googlemail.com"} 
+
+	Subject := "Testing email from golang"
+	bodyMessage := "Sending email using Golang. Yeah"
+
+	sender.SendMail(Dest, Subject, bodyMessage)
+}
