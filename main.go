7e82db3318ef8aa166afd4bb60948f6e47dc39e8
James Tang <tangingw@TuneBox-2.local>
2017-09-12 18:05:22 +0800

    Added Email Message Template

--- main.go
+++ main.go
@@ -5,10 +5,25 @@ func main() {
 	sender := NewSender("<YOUR EMAIL ADDRESS>", "<YOUR EMAIL PASSWORD>")
 
 	//The receiver needs to be in slice as the receive supports multiple receiver
-	Receiver := []string{"abc@gmail.com", "xyz@gmail.com", "larrypage@googlemail.com"} 
+	Receiver := []string{"abc@gmail.com", "xyz@gmail.com", "larrypage@googlemail.com"}
 
-	Subject := "Testing email from golang"
-	bodyMessage := "Sending email using Golang. Yeah"
+	Subject := "Testing HTLML Email from golang"
+	message := `
+	<!DOCTYPE HTML PULBLIC "-//W3C//DTD HTML 4.01 Transitional//EN">
+    <html>
+    <head>
+    <meta http-equiv="content-type" content="text/html"; charset=ISO-8859-1">
+    </head>
+    <body>This is the body<br>
+    <div class="moz-signature"><i><br>
+    <br>
+    Regards<br>
+    Alex<br>
+    <i></div>
+    </body>
+    </html>
+	`
+	bodyMessage := sender.WriteHTMLEmail(Receiver, Subject, message)
 
 	sender.SendMail(Receiver, Subject, bodyMessage)
 }
--- send_mail.go
+++ send_mail.go
@@ -1,8 +1,10 @@
 package main
 
 import (
-	"net/smtp"
+	"bytes"
 	"fmt"
+	"mime/quotedprintable"
+	"net/smtp"
 	"strings"
 )
 
@@ -15,30 +17,26 @@ const (
 	SMTP_SERVER = "smtp.gmail.com"
 )
 
-
 type Sender struct {
-
-	User		string
-	Password	string
+	User     string
+	Password string
 }
-
 
 func NewSender(Username, Password string) Sender {
 
 	return Sender{Username, Password}
 }
 
-
 func (sender Sender) SendMail(Dest []string, Subject, bodyMessage string) {
 
-	msg := 	"From: " + sender.User + "\n" +
-	       	"To: " + strings.Join(Dest, ",") + "\n" +
+	msg := "From: " + sender.User + "\n" +
+		"To: " + strings.Join(Dest, ",") + "\n" +
 		"Subject: " + Subject + "\n" + bodyMessage
 
-	err := smtp.SendMail(SMTP_SERVER + ":587", 
+	err := smtp.SendMail(SMTP_SERVER+":587",
 		smtp.PlainAuth("", sender.User, sender.Password, SMTP_SERVER),
-		sender.User, Dest, []byte(msg))	
-	
+		sender.User, Dest, []byte(msg))
+
 	if err != nil {
 
 		fmt.Printf("smtp error: %s", err)
@@ -46,4 +44,49 @@ func (sender Sender) SendMail(Dest []string, Subject, bodyMessage string) {
 	}
 
 	fmt.Println("Mail sent successfully!")
+}
+
+func (sender Sender) WriteEmail(dest []string, contentType, subject, bodyMessage string) string {
+
+	header := make(map[string]string)
+	header["From"] = sender.User
+
+	receipient := ""
+
+	for _, user := range dest {
+		receipient = receipient + user
+	}
+
+	header["To"] = receipient
+	header["Subject"] = subject
+	header["MIME-Version"] = "1.0"
+	header["Content-Type"] = "text/html; charset=\"utf-8\""
+	header["Content-Transfer-Encoding"] = "quoted-printable"
+	header["Content-Disposition"] = "inline"
+
+	message := ""
+
+	for key, value := range header {
+		message += fmt.Sprintf("%s: %s\r\n", key, value)
+	}
+
+	var encodedMessage bytes.Buffer
+
+	finalMessage := quotedprintable.NewWriter(&encodedMessage)
+	finalMessage.Write([]byte(bodyMessage))
+	finalMessage.Close()
+
+	message += "\r\n" + encodedMessage.String()
+
+	return message
+}
+
+func (sender *Sender) WriteHTMLEmail(dest []string, subject, bodyMessage string) string {
+
+	return sender.WriteEmail(dest, "text/html", subject, bodyMessage)
+}
+
+func (sender *Sender) WritePlainEmail(dest []string, subject, bodyMessage string) string {
+
+	return sender.WriteEmail(dest, "text/plain", subject, bodyMessage)
 }
