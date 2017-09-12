cc513782b21fba9b05f5b3da886f4c1ddce7c66d
James Tang <tangingw@TuneBox-2.local>
2017-09-12 18:07:13 +0800

    Added Email Message Template

--- main.go
+++ main.go
@@ -10,18 +10,18 @@ func main() {
 	Subject := "Testing HTLML Email from golang"
 	message := `
 	<!DOCTYPE HTML PULBLIC "-//W3C//DTD HTML 4.01 Transitional//EN">
-    <html>
-    <head>
-    <meta http-equiv="content-type" content="text/html"; charset=ISO-8859-1">
-    </head>
-    <body>This is the body<br>
-    <div class="moz-signature"><i><br>
-    <br>
-    Regards<br>
-    Alex<br>
-    <i></div>
-    </body>
-    </html>
+	<html>
+	<head>
+	<meta http-equiv="content-type" content="text/html"; charset=ISO-8859-1">
+	</head>
+	<body>This is the body<br>
+	<div class="moz-signature"><i><br>
+	<br>
+	Regards<br>
+	Alex<br>
+	<i></div>
+	</body>
+	</html>
 	`
 	bodyMessage := sender.WriteHTMLEmail(Receiver, Subject, message)
 
